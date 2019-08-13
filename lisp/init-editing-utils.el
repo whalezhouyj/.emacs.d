e3db186c22d62dfe8ba15f4aa8c7d65580698a50
jason zhou <jason@jason-2.local>
2019-08-13 23:33:50 +0800

    fix: iedit warning of key occupied

--- lisp/init-editing-utils.el
+++ lisp/init-editing-utils.el
@@ -173,8 +173,7 @@
 (global-set-key (kbd "C-.") 'set-mark-command)
 (global-set-key (kbd "C-x C-.") 'pop-global-mark)
 
-(when (maybe-require-package 'avy)
-  (global-set-key (kbd "C-;") 'avy-goto-char-timer))
+(when (maybe-require-package 'avy))
 
 (require-package 'multiple-cursors)
 ;; multiple-cursors
